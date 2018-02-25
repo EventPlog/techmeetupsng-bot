894497f531ea1211c47f420ecb851f432a2f2b4b
Innocent <innocent.amadi@andela.com>
2018-02-25 21:29:48 +0100

    refractor copy for help

--- utils/nlEntityProcessors/helpProcessor.js
+++ utils/nlEntityProcessors/helpProcessor.js
@@ -16,7 +16,7 @@ class HelpProcessor extends BaseProcessor {
         {text: "I'm Sarah. I help you discover tech events you might be interested in."},
         {text: "If you are an organizer, I help smoothen the check-in process by letting guests scan a barcode to get in. More efficient than papers ✌🏼."},
         {text: "I also help you send feedback on events you attend or collect feedback on events you organize."},
-        {text: "Type menu at anytime to pull up the main menu"},
+        {text: "Type 'menu' at anytime to pull up the main menu"},
         messages.messageWithButtons(
           "Now how can I help you?",
           [messages.viewEventsButton, messages.createEventButton(recipientId), messages.setPreferencesButton(recipientId)]
