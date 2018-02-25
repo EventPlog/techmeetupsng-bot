802ff24629d82a8c6ceb4d68b390e3f05137094a
Innocent <innocent.amadi@andela.com>
2018-02-25 22:18:38 +0100

    refractor help copy

--- utils/nlEntityProcessors/helpProcessor.js
+++ utils/nlEntityProcessors/helpProcessor.js
@@ -12,13 +12,13 @@ class HelpProcessor extends BaseProcessor {
     // let user = await UserStore.show(recipientId);
     await this.sendPayload(recipientId,
       [
-        {text: "I was wondering when you'll ask 😀"},
+        {text: "I was wondering when you'll ask. 😀"},
         {text: "I'm Sarah. I help you discover tech events you might be interested in."},
         {text: "If you are an organizer, I help smoothen the check-in process by letting guests scan a barcode to get in. More efficient than papers ✌🏼."},
         {text: "I also help you send feedback for events you attend or collect feedback for events you organize."},
-        {text: "Type 'menu' at any point to pull up the main menu"},
-        {text: "Type 'events' at any point to show future tech events"},
-        {text: "Type 'create event' at anytime to have me send you a link to submit an event so we help publicize."},
+        {text: "Type 'menu' at any point to pull up the main menu."},
+        {text: "Type 'events' at any point to show future tech events."},
+        {text: "Type 'create event' at anytime to have me send you a link to submit an event you're organizing so we help publicize."},
         messages.messageWithButtons(
           "Now how can I help you?",
           [messages.viewEventsButton, messages.createEventButton(recipientId), messages.setPreferencesButton(recipientId)]
